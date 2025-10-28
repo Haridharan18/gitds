8d07b94a51d84258fa4d7d98a3f1b11ba4fdbb30
Haridharan18 <dharanhari099@gmail.com>
2025-10-28 13:59:26 +0530

    Initialize Sphinx configuration for documentation

---
+++ conf.py
@@ -0,0 +1,8 @@
+project = 'Data Structures Project - Linked List'
+author = 'Your Name'
+release = '1.0'
+
+extensions = ['recommonmark']
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'sphinx_rtd_theme'
